16be125335e92807177521e68711e23ebdf6b39c
Markus <Markus.SChmidgall@googlemail.com>
2018-10-12 13:23:51 +0200

    Add the menubar and change background of labels depending on successful loading of file

--- DataVisualization/gui.py
+++ DataVisualization/gui.py
@@ -4,10 +4,12 @@ from DataImport import CSVImport
 
 window = Tk()
 window.title('CSV Analytics')
-window.geometry('1280x1024')
+window.geometry("800x600+%d+%d" % (
+    ((window.winfo_screenwidth() / 2.) - (800 / 2.)), ((window.winfo_screenheight() / 2.) - (600 / 2.))))
 NORM_FONT = ("Verdana", 10)
 
 csvfile = None
+defaultbg = window.cget('bg')
 
 
 def callback():
@@ -24,38 +26,51 @@ def callback():
 
 
 def popupmsgsuccess():
-    popup = Tk()
-    popup.wm_title("!")
-    label = Label(popup, text='File geladen!', font=NORM_FONT)
-    label.pack(side="top", fill="x", pady=10)
-    b1 = Button(popup, text="Okay", command=popup.destroy)
-    b1.pack()
+    flash_label.config(text='CSV Datei geladen!', background='green')
 
 
 def popupmsgfail(e):
-    popup = Tk()
-    popup.wm_title("!")
-    label = Label(popup, text=e, font=NORM_FONT)
-    label.pack(side="top", fill="x", pady=10)
-    b1 = Button(popup, text="Okay", command=popup.destroy)
-    b1.pack()
+    flash_label.config(text=e, background='red')
 
 
 def getheaders():
     try:
-        header_label.config(text=CSVImport.getheader(csvfile))
+        headers = CSVImport.getheader(csvfile)
+        header_label.config(text=headers, background=defaultbg, wraplengt=200)
     except TypeError:
-        header_label.config(text='Erst CSV Datei auswählen!')
+        header_label.config(text='Erst CSV Datei auswählen!', background='red')
 
+def donothing():
+    x=0;
+
+
+menubar = Menu(window)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=donothing)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=window.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help Index", command=donothing)
+helpmenu.add_command(label="About...", command=donothing)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
+window.config(menu=menubar)
 
 header_label = Label(window)
+flash_label = Label(window)
 showHeaders_button = Button(window, text='Header anzeigen', command=getheaders)
 getfile_button = Button(window, text='CSV Datei', command=callback)
 exit_button = Button(window, text='Beenden', command=window.quit)
 
 getfile_button.pack()
+flash_label.pack()
 showHeaders_button.pack()
 header_label.pack()
+
 exit_button.pack()
 
 window.mainloop()
