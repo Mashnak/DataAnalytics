e0044ac2a23beea2eeb40723fd36193785573840
Markus <Markus.SChmidgall@googlemail.com>
2018-10-13 13:41:09 +0200

    Create function to determine if column data is int, float or something other

--- DataImport/CSVImport.py
+++ DataImport/CSVImport.py
@@ -6,14 +6,43 @@
 
 # Import von pythoninternen CSV Funktionen
 import csv
-import io
-
-# Globale Variablen Deklaration
 
 
 # Lesen der CSV-Datei
 def getheader(csvfilepath):
+    """
+
+    :param csvfilepath:
+    :return:
+    """
     with open(csvfilepath, 'r') as csvFile:
         csvreader = csv.DictReader(csvFile, delimiter=',')
         returnstring = ', '.join(csvreader.fieldnames)
         return returnstring
+
+
+def getmaxvalue(csvfilepath):
+    """
+
+    :param csvfilepath:
+    :return:
+    """
+    with open(csvfilepath, 'r') as csvFile:
+        csvreader = csv.DictReader(csvFile)
+        next(csvreader)
+        if checkintorfloat(csvreader):
+            maxvalue = max(float(column[0].replace(',', '')) for column in csvreader)
+            return maxvalue
+        else:
+            return False
+
+
+def checkintorfloat(column):
+    """
+
+    :param column:
+    :return:
+    """
+    print(column)
+    value = column
+    return isinstance(value, int) or isinstance(value, float)
